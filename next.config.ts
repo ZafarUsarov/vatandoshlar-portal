af4668dcf66d33b91ba962f017f479cdb3ac77a3
Zafar Usarov <zafarusarov0@gmail.com>
2026-08-08 08:05:12 +0200

    fix(dev): allow local network origin

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ["192.168.0.71"],
+};
 
 export default withNextIntl(nextConfig);
